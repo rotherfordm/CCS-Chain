528a9a0ce4e9fc69bbd7ad953f853722bf90e72a
Rotherford Marmibe <rmarmibe@gmail.com>
2018-08-10 14:38:25 +0800

    Add project files.

---
+++ Blockchain/Block.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Blockchain
+{
+    public class Block
+    {
+        public int index { set; get; }
+        public string previousHash { set; get; }
+        public DateTime timeStamp { set; get; }
+        public string data { set; get; }
+        public string hash { set; get; }
+        public string difficulty { set; get; }
+        public string nonce { set; get; }
+
+        public Block(int index, string previousHash, DateTime timeStamp, string data, string hash, string difficulty, string nonce)
+        {
+            this.index = index;
+            this.previousHash = previousHash;
+            this.timeStamp = timeStamp;
+            this.data = data;
+            this.hash = hash;
+            this.difficulty = difficulty;
+            this.nonce = nonce;            
+        }
+
+
+    }
+}
---
+++ Blockchain/BlockHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blockchain
+{
+    class BlockHandler
+    {
+        public Block getLatestBlock()
+        {
+            Block block = new Block();
+            return block;
+        }
+
+        public string calculateHash()
+        {
+            string hash = "";
+
+            return hash;
+        }
+
+        public Block generateNextBlock()
+        {
+            Block block = new Block();
+            Block previousBlock = getLatestBlock();
+            int nextIndex = previousBlock.index + 1;
+            //TODO: some universal unix time converstion
+            DateTime nextTimestamp = new DateTime();
+
+            return block;
+        }
+    }
+}
