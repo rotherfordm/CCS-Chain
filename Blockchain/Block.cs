fdf2c80313e31f5b6d5b29c7e0841da953f807fc
Rotherford Marmibe <rmarmibe@gmail.com>
2018-08-10 16:41:31 +0800

    updated BlockHandler

--- Blockchain/Block.cs
+++ Blockchain/Block.cs
@@ -6,13 +6,13 @@ namespace Blockchain
     {
         public int index { set; get; }
         public string previousHash { set; get; }
-        public DateTime timeStamp { set; get; }
+        public long timeStamp { set; get; }
         public string data { set; get; }
         public string hash { set; get; }
         public int difficulty { set; get; }
         public int nonce { set; get; }
 
-        public Block(int index, string previousHash, DateTime timeStamp, string data, string hash, int difficulty, int nonce)
+        public Block(int index, string previousHash, long timeStamp, string data, string hash, int difficulty, int nonce)
         {
             this.index = index;
             this.previousHash = previousHash;
@@ -23,7 +23,7 @@ namespace Blockchain
             this.nonce = nonce;     
         }
 
-        public Block(int index, string previousHash, DateTime timeStamp, string data, string hash)
+        public Block(int index, string previousHash, long timeStamp, string data, string hash)
         {
             this.index = index;
             this.previousHash = previousHash;
--- Blockchain/BlockHandler.cs
+++ Blockchain/BlockHandler.cs
@@ -1,25 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Security.Cryptography;
+using System.Linq;
 
 namespace Blockchain
 {
     public class BlockHandler
     {
-        int difficulty = 4;
+        public List<Block> blockChain { set; get; }
+        public int difficulty { set; get; }
+
+        BlockHandler()
+        {
+            blockChain.Add(getGenesisBlock());
+            difficulty = 4;
+        }
 
         public Block getLatestBlock()
         {
-            Block block;
-            return block;
+            return blockChain[blockChain.Count - 1];
         }
 
-        public string calculateHash(int nextIndex, string previousBlockHash, DateTime nextTimestamp, string blockData)
+        public string calculateHash(int nextIndex, string previousBlockHash, long nextTimestamp, string blockData)
         {
             //TODO: Implement SHA256 hashing use C# hash
             string hash = "";
+            throw new NotImplementedException();
+        }
 
-            return hash;
+        public string calculateHash(int nextIndex, string previousBlockHash, long nextTimestamp, string blockData, int nonce)
+        {
+            //TODO: Implement SHA256 hashing use C# hash
+            string hash = "";
+            throw new NotImplementedException();
+        }
+
+        public string calculateHashForBlock(Block block)
+        {
+            throw new NotImplementedException();
         }
 
         public Block generateNextBlock(string blockData)
@@ -27,15 +46,69 @@ namespace Blockchain
             Block previousBlock = getLatestBlock();
             int nextIndex = previousBlock.index + 1;
             //TODO: some universal unix time converstion
-            DateTime nextTimestamp = new DateTime();
+            long nextTimestamp = new long();
+            nextTimestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
             string nextHash = calculateHash(nextIndex, previousBlock.hash, nextTimestamp, blockData);
             return new Block(nextIndex, previousBlock.hash, nextTimestamp, blockData, nextHash);
         }
 
+        public void addBlock(Block newBlock)
+        {
+            if (isValidNewBlock(newBlock, getLatestBlock()))
+            {
+                blockChain.Add(newBlock);
+            }
+        }
+
+        public bool isValidNewBlock(Block newBlock, Block previousBlock)
+        {
+            if (previousBlock.index + 1 != newBlock.index)
+            {
+                Console.WriteLine("Invalid Index");
+                return false;
+            }
+            else if (previousBlock.hash != newBlock.previousHash)
+            {
+                Console.WriteLine("Invalid Previous Hash");
+                return false;
+            }
+            else if (calculateHashForBlock(newBlock) != newBlock.hash)
+            {
+                Console.WriteLine(newBlock.hash.GetType());
+                Console.WriteLine("Invalid Hash: {0} {1}", calculateHashForBlock(newBlock), newBlock.hash);
+                return false;
+            }
+            return true;
+        }
+
         public Block getGenesisBlock()
         {
-            return new Block(0, "0", DateTime.Now, "my genesis block!!", 
+            return new Block(0, "0", DateTimeOffset.UtcNow.ToUnixTimeSeconds(), "my genesis block!!", 
                 "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7", 0, 0);
+        }
+
+        public Block mineBlock(string blockData)
+        {
+            Block previousBlock = getLatestBlock();
+            int nextIndex = previousBlock.index + 1;
+            int nonce = 1;
+            long nextTimestamp = new long();
+            nextTimestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds() / 1000;
+            string nextHash = calculateHash(nextIndex, previousBlock.hash, nextTimestamp, blockData, nonce);
+
+            string noOfZeros = new string('0', difficulty); //TODO: test this
+            while (nextHash.Substring(0, difficulty) != noOfZeros)
+            {
+                nonce++;
+                nextTimestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds() / 1000;
+                nextHash = calculateHash(nextIndex, previousBlock.hash, nextTimestamp, blockData, nonce);
+                Console.WriteLine("\"index\":" + nextIndex + ",\"previoushash\":" + previousBlock.hash +
+                "\"timestamp\":" + nextTimestamp + ",\"data\":" + blockData +
+                ",\x1b[33mhash: " + nextHash + "\x1b[0m," +      "\"difficulty\":" + difficulty +
+                "\x1b[33mnonce: " + nonce + "\x1b[0m");
+            }
+            return new Block(nextIndex, previousBlock.hash, nextTimestamp, blockData, nextHash, difficulty, nonce);
+            
         }
     }
 }
