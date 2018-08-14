14cfd8c4e100059fbd3b3e53a15c7abb54d265f1
Rotherford Marmibe <rmarmibe@gmail.com>
2018-08-14 10:49:51 +0800

    added merkletree base classes -pls fix shan

---
+++ CCSblockchain/Models/MerkleTree.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace MerkleTree
+{
+    class MerkleTree
+    {
+        /* 
+         * 
+         * `The Merkle Tree is a special kind of Binary Tree that allows the user to prevent 
+        information malleability and preserve integrity by using cryptographically secure
+        hash functions. By providing leaves you create a root by concatination and hashingself.
+        If the root is mutated it means that the data inside the leaves was changed. You can also
+        derive cryptographic proofs that a piece of data is inside the tree`
+
+        Comments:
+           Complexities - This part can be improved by doing couple of modifications 
+         
+             */
+
+        public MerkleTree()
+        {
+
+        }
+
+        public Node BuildRoot(List<Node> nodes)
+        {
+            Node root = new Node();
+            if (!(nodes == null))
+            {
+                return null;
+            }
+            return root;
+        }
+
+        public bool Contains(object value)
+        {
+            if (value == null) { return false; }
+
+            return false;
+        }
+
+        public bool Find(Node node, byte value)
+        {
+            if (node.value == null)
+            {
+                return false;
+            }
+            if (node.value == value)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public List<Nodes> RequestProof(byte value)
+        {
+
+            return 
+        }
+
+    }
+}
---
+++ CCSblockchain/Models/Node.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerkleTree
+{
+    class Node
+    {
+        public byte value { set; get; }
+        public Node left { set; get; }
+        public Node right { set; get; }
+        public Node root { set; get; }
+
+
+        public Node()
+        {
+
+        }
+
+        /*
+         *  `Merkle Node constructor. Used for storing the left and right node pointersself.`
+
+        Args:
+            item (bytes): Bytes object that represents the hashed value that resides in the current node
+            left (Node): Reference to the left subtree or a None value if current node is leaf
+            right (Node): Reference to the right subtree or a None value if current node is leaf
+         */
+        public Node(byte item, Node left, Node right)
+        {
+            this.value = item;
+            this.left = left;
+            this.right = right;
+
+        }
+    }
+}
